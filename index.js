72ce0c79ec4973e913fa202577a06e7594218ef7
Vlad Lutsenko <lutsenko.vladyslav@gmail.com>
2020-02-19 21:57:57 +0200

    fixed js

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ const markupRender = function() {
     (acc, item) =>
       acc +
       ` <li class="gallery__item">
-
+      <a class="gallery__link" href="${item.original}">
     <img
       class="gallery__image"
       src="${item.preview}"
       data-source="${item.original}"
       alt="${item.description}"
     />
-  
+    </a>
   </li> `,
     ""
   );
@@ -80,6 +80,3 @@ const closeImgByEsc = function(event) {
   }
 };
 window.addEventListener("keydown", closeImgByEsc);
-
-// Пролистывание изображений галереи в открытом модальном окне
-// клавишами "влево" и "вправо".
