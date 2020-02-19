c5aaeef23ecf1c35054ebf21baf609c4293b3123
Vlad Lutsenko <lutsenko.vladyslav@gmail.com>
2020-02-19 21:30:53 +0200

    some additional tasks added

--- index.js
+++ index.js
@@ -10,17 +10,14 @@ const markupRender = function() {
     (acc, item) =>
       acc +
       ` <li class="gallery__item">
-  <a
-    class="gallery__link"
-    href="${item.original}"
-  >
+
     <img
       class="gallery__image"
       src="${item.preview}"
       data-source="${item.original}"
       alt="${item.description}"
     />
-  </a>
+  
   </li> `,
     ""
   );
@@ -39,6 +36,9 @@ const lightboxImage = document.querySelector(".lightbox__image");
 
 const openImg = function(event) {
   event.preventDefault();
+  if (event.target === event.currentTarget) {
+    return;
+  }
   const newUrl = event.target.dataset.source;
 
   lightboxImage.src = newUrl;
@@ -60,3 +60,26 @@ const closeImg = function() {
 };
 
 closeBtn.addEventListener("click", closeImg);
+
+///////////////------------Дополнительно------------/////////////////////////////
+//Закрытие модального окна по клику на div.lightbox__overlay.
+const closeImgByOverlayClick = function(event) {
+  if (event.target !== lightboxImage) {
+    closeImg();
+  }
+  return;
+};
+
+lightbox.addEventListener("click", closeImgByOverlayClick);
+
+// Закрытие модального окна по нажатию клавиши ESC
+
+const closeImgByEsc = function(event) {
+  if (event.code === "Escape") {
+    closeImg();
+  }
+};
+window.addEventListener("keydown", closeImgByEsc);
+
+// Пролистывание изображений галереи в открытом модальном окне
+// клавишами "влево" и "вправо".
